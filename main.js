3d22d3af2f79701ae42185339ace40f3fe835ede
uy <a.normurzaev20@gmail.com>
2026-03-19 23:07:09 +0500

    13 masalani yakunladim

--- main.js
+++ main.js
@@ -103,4 +103,11 @@
 // colors.splice(1, 1)
 // console.log(colors);
 
-130
+
+// 13.masala
+
+// let nums = [5, 2, 9, 1];
+//  nums.sort(function(a, b){
+//     return a - b;
+//  })
+// console.log(nums);
